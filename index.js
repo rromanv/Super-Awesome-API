f6af023e698428dbd16d6f894beff3b9d6dc8f81
Rodolfo Roman <rromanv@gmail.com>
2019-12-02 00:31:28 -0600

    Production Ready

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 import indexRouter from './src/routes/index'
 import apiRouter from './src/routes/api'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors({
+  origin: process.env.ORIGIN
+}))
+app.use(morgan('combined'))
 
 app.use('/', indexRouter)
 app.use('/api', apiRouter)
